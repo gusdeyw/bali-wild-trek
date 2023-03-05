3fde11c4a6a650432b856e3f5992e1f15c9fb054
Arta Pradana <artapradana908@gmail.com>
2023-03-06 02:58:37 +0800

    bru responsive hp dan blum lengkap, bsk lnjut lgi

--- index.php
+++ index.php
@@ -5,14 +5,76 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,300&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Patua+One&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Courgette&display=swap" rel="stylesheet">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
   <script src="https://cdn.tailwindcss.com"></script>
-  <title>Bali Wild Track</title>
+  <title>Bali Wild Trek</title>
 </head>
 
 <body>
-  <h1 class="text-3xl font-bold underline text-clifford">
-    Hello Boorr!
-  </h1>
+  <div class="bg-bottom">
+    <img class="h-[39.1em] w-screen" src="admin/assets/img/5.jpg" alt="Sawah Bali">
+    <div class="absolute top-[12.5em] w-full text-white font-bold mx-auto">
+      <h1 class="md:text-7xl text-5xl font-inter text-center">Bali Wild Trek</h1>
+      <h3 class="text-center md:text-2xl pt-5 font-roboto">Unlimited Adventure in Bali Island</h3>
+    </div>
+    <div class="absolute bottom-5 sm:left-[47.5%] left-[46.5%]">
+      <svg xmlns="http://www.w3.org/2000/svg" class="w-[30px] h-[30px] md:w-[40px] md:h-[40px]" fill="white" class="bi bi-arrow-down-circle" viewBox="0 0 16 16">
+        <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v5.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V4.5z" />
+      </svg>
+    </div>
+  </div>
+
+  <div class="h-screen w-full mx-auto px-12 mt-14">
+    <div class="">
+      <div class="ml-8">
+        <h1 class="ml-14 md:ml-9 font-bold text-[#1FACCB] text-lg font-[patua]">Tour the</h1>
+        <h1 class="ml-8 md:ml-5 font-bold text-[#1FACCB] text-lg font-[courgette]">Playing On A Tour</h1>
+        <h1 class="font-[pacifico] md:ml-0 font-bold text-[#1FACCB] text-lg">Work While Relaxing</h1>
+      </div>
+      <div class="text-sm mt-2 font-[arial]">
+        <p>We are a travel agent who specializes in offering best price, quality and services for your Bali travel. We provide all inclusive packages and activities around the world with experienced guide. Our professional team will guide you through the best activities to keep your vacation fun, safe and memorable. We are here to help you plan the perfect Balinese vacation. Enjoy the beauty with amazing photos and videos by our professional photographers, enjoy sunrise yoga on a tropical island come alive at sunrise</p>
+      </div>
+    </div>
+    <div class="mt-14 w-full mx-7 sm:mx-0 sm:grid sm:grid-cols-2 sm:gap-x-2 md:grid md:gap-x-0 md:ml-[4%] lg:grid lg:grid-cols-3 lg:ml-3">
+      <div class="">
+        <img class="w-[276px] h-[178px]" src="admin/assets/img/6.jpg" alt="Cooking Class">
+        <button class="w-[180px] h-[46px] mt-3 mx-11 bg-white border-[1px] border-solid border-[#1FACCB] rounded-md">
+          <a href="#" class="text-[#1FACCB] font-bold font-roboto">Cooking Class</a>
+        </button>
+      </div>
+      <div class="mt-7 sm:mt-0">
+        <img class="w-[276px] h-[178px]" src="admin/assets/img/8.jpg" alt="Purifiation">
+        <button class="w-[180px] h-[46px] mt-3 mx-11 bg-white border-[1px] border-solid border-[#1FACCB] rounded-md">
+          <a href="#" class="text-[#1FACCB] font-bold font-roboto">Purifiation</a>
+        </button>
+      </div>
+      <div class="mt-7 sm:mt-6 sm:w-full sm:ml-[50%] lg:mt-0 lg:ml-0">
+        <img class="w-[276px] h-[178px]" src="admin/assets/img/7.jpeg" alt="Trekking">
+        <button class="w-[180px] h-[46px] mt-3 mx-11 bg-white border-[1px] border-solid border-[#1FACCB] rounded-md">
+          <a href="#" class="text-[#1FACCB] font-bold font-roboto">Trekking</a>
+        </button>
+      </div>
+    </div>
+    <div class="mt-7 px-[4rem] sm:w-full sm:ml-[19.5%] md:ml-[24.5%]">
+      <button class="w-[200px] h-[47px] bg-[#1FACCB] rounded-[5px] border-[1px] shadow-sm border-solid border-black">
+        <a href="#" class="text-white font-bold">Activity Tour</a>
+      </button>
+    </div>
+  </div>
 </body>
 
 </html>
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  theme: {
+    extend: {
+      zIndex: {
+        100: '999999999',
+      },
+      fontFamily: {
+        inter: ['Inter'],
+        roboto: ['Roboto'],
+        patua: ['Patua'],
+        courgette: ['Courgette'],
+        pacifico: ['Pacifico'],
+      },
+    },
+  },
+};
