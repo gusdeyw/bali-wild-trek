c72581d01a84598468c96bdfecfefa6e6e8a6fe0
Gusde Widnyana <widnyana.bagus@gmail.com>
2023-03-06 21:17:56 +0800

    Revisi Admin Panel

--- activity-list.php
+++ activity-list.php
@@ -1,28 +1,3 @@
-<?php
-session_start();
-include "admin/dbconnect.php";
-$id = $_GET["id"];
-$query = "SELECT * FROM activity WHERE id = " . $id;
-
-$result = mysqli_query($koneksi, $query);
-$title = '';
-$description = '';
-$currency = '';
-$price = '';
-$uom = '';
-$benefit = '';
-
-while ($row = mysqli_fetch_array($result)) {
-    $title = $row['title'];
-    $description = $row['description'];
-    $currency = $row['currency'];
-    $price = $row['price'];
-    $uom = $row['uom'];
-    $benefit = $row['benefit'];
-    $images = $row['images'];
-    $images = explode(',', $images);
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -30,79 +5,6 @@ while ($row = mysqli_fetch_array($result)) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-    .swiper {
-        width: 100%;
-        height: 100%;
-    }
-
-    .swiper-slide {
-        text-align: center;
-        font-size: 18px;
-        background: #fff;
-
-        /* Center slide text vertically */
-        display: -webkit-box;
-        display: -ms-flexbox;
-        display: -webkit-flex;
-        display: flex;
-        -webkit-box-pack: center;
-        -ms-flex-pack: center;
-        -webkit-justify-content: center;
-        justify-content: center;
-        -webkit-box-align: center;
-        -ms-flex-align: center;
-        -webkit-align-items: center;
-        align-items: center;
-    }
-
-    .swiper-slide img {
-        display: block;
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-
-    .swiper {
-        width: 100%;
-        height: 300px;
-        margin-left: auto;
-        margin-right: auto;
-    }
-
-    .swiper-slide {
-        background-size: cover;
-        background-position: center;
-    }
-
-    .mySwiper2 {
-        height: 100%;
-        width: 100%;
-    }
-
-    .mySwiper {
-        height: 100%;
-        box-sizing: border-box;
-        padding: 10px 0;
-    }
-
-    .mySwiper .swiper-slide {
-        width: 25%;
-        height: 100%;
-        opacity: 0.4;
-    }
-
-    .mySwiper .swiper-slide-thumb-active {
-        opacity: 1;
-    }
-
-    .swiper-slide img {
-        display: block;
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-    </style>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.css" />
@@ -152,66 +54,11 @@ while ($row = mysqli_fetch_array($result)) {
         </div>
     </div>
     <!-- content -->
-    <div class="w-10/12 mx-auto">
-        <!-- Activity Tittle  -->
-        <div class="mt-12">
-            <h3 class="font-bold text-[#63B8C6] text-3xl"><?= $title?></h3>
-            <h4 class="font-normal text-blue-500 text-2xl">Playing On A Tour</h4>
-        </div>
-        <div class="grid grid-cols-5 mt-7">
-            <div class="col-span-5 md:col-span-3">
-                <div class="mb-4 h-[13rem] md:h-[35rem]">
-                    <div style="--swiper-navigation-color: #fff; --swiper-pagination-color: #fff"
-                        class="mt-5 swiper mySwiper2 swiper-initialized swiper-horizontal">
-                        <div class=" swiper-wrapper" id="swiper-wrapper-822f4d287c357ecc" aria-live="polite"
-                            style="transform: translate3d(0px, 0px, 0px);">
-                            <?php foreach ($images as $link) { ?>
-                            <div class="swiper-slide swiper-slide-active" role="group" aria-label="1 / 14"
-                                style="width: 1053px; margin-right: 10px;"><img src="<?php echo $link;?>">
-                            </div>
-                            <?php }?>
-                        </div>
-                        <div class="swiper-button-next" tabindex="0" role="button" aria-label="Next slide"
-                            aria-controls="swiper-wrapper-822f4d287c357ecc" aria-disabled="false"></div>
-                        <div class="swiper-button-prev swiper-button-disabled" tabindex="-1" role="button"
-                            aria-label="Previous slide" aria-controls="swiper-wrapper-822f4d287c357ecc"
-                            aria-disabled="true"></div>
-                        <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
-                    </div>
-                </div>
-                <div class="h-32 lg:h-40">
-                    <div thumbsslider=""
-                        class="swiper mySwiper swiper-initialized swiper-horizontal swiper-free-mode swiper-watch-progress swiper-thumbs">
-                        <div class="swiper-wrapper gap-7" id="swiper-wrapper-2c3bc282e24c118b" aria-live="polite"
-                            style="transform: translate3d(0px, 0px, 0px);">
-                            <?php foreach ($images as $link) { ?>
-                            <div class="swiper-slide swiper-slide-visible swiper-slide-active swiper-slide-thumb-active"
-                                role="group" style="width: 255.75px; margin-right: 10px;"><img
-                                    src="<?php echo $link;?>">
-                            </div>
-                            <?php }?>
-                        </div>
-                        <span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span>
-                    </div>
-                </div>
-                <div class="w-full mt-2">
-                    <?= $description ?>
-                </div>
-            </div>
-            <div class="col-span-5 md:col-span-2 mt-6 md:mt-0 md:ml-6">
-                <p class="text-3xl font-bold"><?=$currency?> <?=$price?>/<?=$uom?></p>
-                <p class="text-xl">Benefit :</p>
-                <?=$benefit?> <br>
-                <button class="px-8 py-2 mt-7 shadow-md rounded-lg bg-[#63B8C6] text-white font-medium">Book
-                    Now</button>
-            </div>
-        </div>
-        <div class="w-96 mx-auto text-center mt-10">
-            <p class="text-[#63B8C6] font-bold text-2xl">Other Activity</p>
-        </div>
+    <div class="w-11/12 mx-auto my-12">
         <div class="grid grid-cols-3 gap-3 mt-3">
             <?php
-            $query = "SELECT * FROM activity WHERE is_active = 1 ORDER BY RAND() LIMIT 3";
+            include "admin/dbconnect.php";
+            $query = "SELECT * FROM activity WHERE is_active = 1 ORDER BY RAND()";
 
             $result = mysqli_query($koneksi, $query);
             $title = '';
@@ -236,9 +83,9 @@ while ($row = mysqli_fetch_array($result)) {
                     break;
                 }
             ?>
-            <div class="col-span-3 md:col-span-1">
+            <div class="md:col-span-1 col-span-3">
                 <div class="bg-white filter rounded-2xl drop-shadow-lg col-span-1">
-                    <a href="activity.php?id=<?= $id ?>" class="">
+                    <a href="activity.php?id=<?=$id?>" class="">
                         <img class="object-cover h-56 w-full rounded-t-2xl" loading="lazy" src="<?=$image_link2?>">
                         <p class="text-center font-bold text-[#63B8C6] text-xl"><?= $title?></p>
                         <div class="flex justify-center p-4">
@@ -252,6 +99,7 @@ while ($row = mysqli_fetch_array($result)) {
             ?>
         </div>
     </div>
+
     <!-- footer  -->
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" class="-mb-40">
         <path fill="#63B8C6" fill-opacity="1"
--- admin/update_process.php
+++ admin/update_process.php
@@ -14,7 +14,7 @@ if ($_POST['is_active']) {
 
 include "dbconnect.php";
 // $query = "INSERT INTO activity (title, description, currency, price, uom, benefit, is_active) VALUES ('$title', '$description', '$currency', $price, '$uom', '$benefit', $is_active)";
-$query = "UPDATE activity SET title = '$title', description = '$description', currency = '$currency', price = $price, uom = '$uom' , benefit = '$uom', is_active = $is_active WHERE id = $id";
+$query = "UPDATE activity SET title = '$title', description = '$description', currency = '$currency', price = $price, uom = '$uom' , benefit = '$benefit', is_active = $is_active WHERE id = $id";
 $result = mysqli_query($koneksi, $query);
 
 if ($result) {
