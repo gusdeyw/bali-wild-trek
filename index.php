99ddf48837e5ca8446ecd785d329c76c9130235e
Arta Pradana <artapradana908@gmail.com>
2023-03-06 15:48:28 +0800

    update terbaru

--- index.php
+++ index.php
@@ -38,43 +38,73 @@
     </div>
   </div>
 
-  <div class="h-screen w-full mx-auto px-12 mt-14">
-    <div class="">
-      <div class="ml-8">
-        <h1 class="ml-14 md:ml-9 font-bold text-[#1FACCB] text-lg font-[patua]">Tour the</h1>
-        <h1 class="ml-8 md:ml-5 font-bold text-[#1FACCB] text-lg font-[courgette]">Playing On A Tour</h1>
-        <h1 class="font-[pacifico] md:ml-0 font-bold text-[#1FACCB] text-lg">Work While Relaxing</h1>
+  <div class="h-screen w-full mx-auto px-12 mt-14 xl:px-28">
+    <div class="lg:flex lg:justify-around">
+      <div class="ml-8 lg:w-full lg:mt-3 lg:ml-28 xl:mx-12">
+        <h1 class="ml-14 md:ml-9 lg:absolute xl:pl-32 lg:pl-5 font-bold text-[#1FACCB] text-lg lg:text-2xl font-[patua]">Tour the</h1>
+        <h1 class="ml-8 md:ml-5 lg:absolute xl:pl-[6.5rem] lg:pt-9 lg:-ml-[3.5rem] font-bold text-[#1FACCB] text-lg lg:text-2xl font-[courgette]">Playing On A Tour</h1>
+        <h1 class="font-[pacifico] md:ml-0 lg:absolute xl:pt-[4.5rem] xl:pl-[6.5rem] lg:pt-[4.5rem] lg:pl-0 lg:-ml-[6rem] font-bold text-[#1FACCB] lg:text-2xl text-lg">Work While Relaxing</h1>
       </div>
-      <div class="text-sm mt-2 font-[arial]">
+      <div class="text-sm mt-2 font-[arial] xl:px-28 xl:-ml-20 lg:text-base">
         <p>We are a travel agent who specializes in offering best price, quality and services for your Bali travel. We provide all inclusive packages and activities around the world with experienced guide. Our professional team will guide you through the best activities to keep your vacation fun, safe and memorable. We are here to help you plan the perfect Balinese vacation. Enjoy the beauty with amazing photos and videos by our professional photographers, enjoy sunrise yoga on a tropical island come alive at sunrise</p>
       </div>
     </div>
-    <div class="mt-14 w-full mx-7 sm:mx-0 sm:grid sm:grid-cols-2 sm:gap-x-2 md:grid md:gap-x-0 md:ml-[4%] lg:grid lg:grid-cols-3 lg:ml-3">
-      <div class="">
-        <img class="w-[276px] h-[178px]" src="admin/assets/img/6.jpg" alt="Cooking Class">
-        <button class="w-[180px] h-[46px] mt-3 mx-11 bg-white border-[1px] border-solid border-[#1FACCB] rounded-md">
+    <div class="mt-14 w-full mx-7 sm:mx-0 sm:grid sm:grid-cols-2 sm:gap-x-2 md:grid md:gap-x-0 lg:grid lg:grid-cols-3 lg:gap-x-1">
+      <div class="md:w-full">
+        <img class="w-[276px] h-[178px] md:w-[316px] md:h-[200px] xl:w-[382px] xl:h-[274px]" src="admin/assets/img/6.jpg" alt="Cooking Class">
+        <button class="w-[180px] h-[46px] mt-3 mx-11 bg-white border-[1px] md:mx-16 xl:mx-[5.5rem] border-solid border-[#1FACCB] rounded-md">
           <a href="#" class="text-[#1FACCB] font-bold font-roboto">Cooking Class</a>
         </button>
       </div>
-      <div class="mt-7 sm:mt-0">
-        <img class="w-[276px] h-[178px]" src="admin/assets/img/8.jpg" alt="Purifiation">
-        <button class="w-[180px] h-[46px] mt-3 mx-11 bg-white border-[1px] border-solid border-[#1FACCB] rounded-md">
+      <div class="mt-7 sm:mt-0 md:w-full">
+        <img class="w-[276px] h-[178px] md:w-[316px] md:h-[200px] xl:w-[382px] xl:h-[274px]" src="admin/assets/img/8.jpg" alt="Purifiation">
+        <button class="w-[180px] h-[46px] mt-3 mx-11 bg-white border-[1px] xl:mx-[5.5rem] border-solid md:mx-16 border-[#1FACCB] rounded-md">
           <a href="#" class="text-[#1FACCB] font-bold font-roboto">Purifiation</a>
         </button>
       </div>
       <div class="mt-7 sm:mt-6 sm:w-full sm:ml-[50%] lg:mt-0 lg:ml-0">
-        <img class="w-[276px] h-[178px]" src="admin/assets/img/7.jpeg" alt="Trekking">
-        <button class="w-[180px] h-[46px] mt-3 mx-11 bg-white border-[1px] border-solid border-[#1FACCB] rounded-md">
+        <img class="w-[276px] h-[178px] md:w-[316px] md:h-[200px] xl:w-[382px] xl:h-[274px]" src="admin/assets/img/7.jpeg" alt="Trekking">
+        <button class="w-[180px] h-[46px] mt-3 mx-11 bg-white border-[1px] border-solid md:mx-16 xl:mx-[5.5rem] border-[#1FACCB] rounded-md">
           <a href="#" class="text-[#1FACCB] font-bold font-roboto">Trekking</a>
         </button>
       </div>
     </div>
-    <div class="mt-7 px-[4rem] sm:w-full sm:ml-[19.5%] md:ml-[24.5%]">
-      <button class="w-[200px] h-[47px] bg-[#1FACCB] rounded-[5px] border-[1px] shadow-sm border-solid border-black">
-        <a href="#" class="text-white font-bold">Activity Tour</a>
+    <div class="mt-7 mb-2 px-[4rem] sm:pl-[10.5em] md:pl-[14em] lg:pl-[22.9em] xl:pl-[23.5em] 2xl:pl-[32.4em]">
+      <button class="w-[200px] h-[47px] xl:w-[380px] xl:h-[70px] bg-[#1FACCB] rounded-[5px] border-[1px] mx-auto shadow-sm border-solid border-black">
+        <a href="#" class="text-white xl:text-2xl font-bold">Activity Tour</a>
       </button>
     </div>
   </div>
+
+  <div class="mt-[39em] sm:mt-[20em] lg:mt-[4em] w-full h-screen">
+    <img src="admin/assets/img/3.3.jpg" class="h-[28em] md:w-full md:h-[35rem]" alt="beach">
+    <div class="absolute px-16 -mt-[25rem] sm:left-64 md:left-52 md:-mt-[30rem] lg:left-[26em] xl:left-[40em] 2xl:left-[55em]">
+      <h1 class="font-bold text-white pl-9 sm:pl-16 text-lg font-[pacifico] md:text-2xl">Very Beautiful Scenery</h1>
+      <div class="w-[25em] sm:w-[30em] sm:h-56 h-64 bg-[#1FACCB] p-3 sm:p-4 text-xs font-[Aleo] font-bold text-white md:text-sm md:h-72">
+        <p class="mb-1">We are a travel agency that specialized in Bali Island trip. Our mission is to offer you the best experience of travelling Bali. We do have experienced tour guide, competitive price and flexibility to suit your needs, giving you a memorable trip that never grows old.</p>
+        <p>Bali Island Travel is an experienced travel agent with many years of experience in helping travelers to enjoy the best of bali island. Our team can arrange tours, flights and hotels that suit your needs and budget - whether you're looking for a relaxing retreat or want to see some of Indonesia's top attractions. Explore this fascinating island on our tailored tour, which includes all details such as transportation and accommodations.</p>
+      </div>
+      <button class="absolute w-[100px] h-[30px] -mt-3 ml-[10rem] sm:ml-[13.5rem] bg-[#1FACCB] md:ml-[17.5rem]">
+        <a href="#" class="text-white font-bold font-roboto">Next</a>
+      </button>
+      <button class="absolute w-[30px] h-[30px] -mt-3 ml-[16.5rem] sm:ml-[20rem]  md:ml-[24rem] bg-[#1FACCB]">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-arrow-right" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+        </svg>
+      </button>
+    </div>
+  </div>
+
+
+  <div class="w-full h-screen px-12 -mt-[7em] md:mt-0 md:flex justify-around">
+    <div class="font-[arial] text-sm md:text-base md:w-60 md:h-60 md:mt-5 md:-mr-28 lg:w-[36rem] lg:h-60 lg:-mr-16 lg:mt-24 xl:-mr-20">
+      <p>We are an expert travel agent and guide in bali island. We have been offering service for more than 10 years, as well as offering competitive price and benefits to our clients. We can help you experience the true beauty of Bali Island, with lots of activity around the island with experienced guides, who are familiar with local culture, history and tradition</p>
+    </div>
+    <div class="md:w-64 md:h-64 lg:w-72 lg:h-72 xl:-ml-28">
+      <img src="admin/assets/img/9.jpg" alt="Bali">
+    </div>
+  </div>
+
 </body>
 
 </html>
