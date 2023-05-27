03b018d218f8b16c33c6ea0789d410d9026e511a
Gusde Widnyana <widnyana.bagus@gmail.com>
2023-05-27 12:33:10 +0800

    Fixing Button Name

--- index.php
+++ index.php
@@ -183,7 +183,7 @@ include "header/header.php";
                             <button class="px-6 py-1 bg-[#1FACCB]">
                                 Next
                             </button>
-                            <button class="px-3 py-1 bg-[#1FACCB]" role="button" type="button">
+                            <button class="px-3 py-1 bg-[#1FACCB]" role="button" type="button" aria-label="See More Button" title="See More">
                                 <svg xmlns="https://www.w3.org/2000/svg" width="20" height="20" fill="white" class="bi bi-arrow-right" viewBox="0 0 16 16">
                                     <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
                                 </svg>
